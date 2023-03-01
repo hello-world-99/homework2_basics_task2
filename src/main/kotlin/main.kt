eb56209ff6cf4f3ef3a8c0b214fd52c74405c023
hello-world-99 <69188993+hello-world-99@users.noreply.github.com>
2023-03-01 09:25:03 +0300

    1

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,8 @@
 fun main() {
     while (true) {
         val likes = readln()
-        if
+        val result: String =
+            if(likes.last()=='1'&&(if(likes.length>1) likes[likes.length-2]!='1' else true)) "Понравилось $likes человеку" else "Понравилось $likes людям"
+        println(result)
     }
 }
